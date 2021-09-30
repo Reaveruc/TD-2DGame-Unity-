68dabe7f2c9c4c6e97ce47ece85999758b1adcac
Ivan <reaveruc@yandex.ru>
2021-09-30 19:00:27 +0700

    Assets

---
+++ Assets/PlaceMonster.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceMonster : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject monsterPrefab;
+    private GameObject monster;
+    private GameManagerBehavior gameManager;
+
+    private void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManagerBehavior>();
+    }
+
+    private bool CanPlaceMonster()
+    {
+        int cost = monsterPrefab.GetComponent<MonsterData>().levels[0].cost;
+        return monster == null && gameManager.Gold >= cost;
+    }
+
+
+    private bool CanUpgradeMonster()
+    {
+        if (monster != null)
+        {
+            MonsterData monsterData = monster.GetComponent<MonsterData>();
+            MonsterLevel nextLevel = monsterData.GetNextLevel();
+            if (nextLevel != null)
+            {
+                return gameManager.Gold >= nextLevel.cost;
+            }
+        }
+        return false;
+    }
+    void OnMouseUp()
+    {
+        //2
+        if (CanPlaceMonster())
+        {
+            //3
+            monster = (GameObject)
+              Instantiate(monsterPrefab, transform.position, Quaternion.identity);
+            //4
+            AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.PlayOneShot(audioSource.clip);
+            gameManager.Gold -= monster.GetComponent<MonsterData>().CurrentLevel.cost;
+
+        }
+        else if (CanUpgradeMonster())
+        {
+            monster.GetComponent<MonsterData>().IncreaseLevel();
+            AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.PlayOneShot(audioSource.clip);
+              gameManager.Gold -= monster.GetComponent<MonsterData>().CurrentLevel.cost;
+
+        }
+
+    }
+}
